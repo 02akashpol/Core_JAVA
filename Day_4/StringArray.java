980904990e34a051e4e2ed27714520a0b35bb0f3
Akash_Pol <65165883+02akashpol@users.noreply.github.com>
2020-09-21 18:04:24 +0530

    Assignment files are uploaded.

--- Day_4/StringArray.java
+++ Day_4/StringArray.java
@@ -6,6 +6,7 @@ public class StringArray{
 
 		Scanner sc = new Scanner(System.in);
 
+		/*
 		String strArray[] = new String[] {"A", "B", "C", "D", "E"};
 
 		System.out.println("Elements of string type array : ");
@@ -13,6 +14,23 @@ public class StringArray{
 		for(String trav : strArray) {
 
 			System.out.print(" "+trav);
+		}
+		*/
+		
+		int size = sc.nextInt();
+		
+		String arr[] = new String[size];
+		
+		sc.nextLine();
+		
+		for(int i=0; i<arr.length; i++) {
+			
+			arr[i] = sc.nextLine();
+		}
+		
+		for(String trav : arr) {
+			
+			System.out.println(trav);
 		}
 	}
 }
